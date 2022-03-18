82e36ea87177338c81f7e15048063391b8f00f1c
XotEmBotZ <xotembotzop@gmail.com>
2022-03-18 17:56:05 +0530

    Added welcome text and done some basic styling in it

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import style from "../styles/index.module.css"
+import Image from "next/image"
+
+
 export default function Home() {
   return (
-    <h1>Hi</h1>
+    <>
+      <section id="wolcome" className={style.welcomeSection}>
+        <div className={style.welcomeImage}>
+          {/* <Image/> */}
+        </div>
+        <div className={style.welcomeText}>
+          <span className={style.welcomeText}>Welcome,</span>
+          I am a responsive web designer. I know HTML, CSS, Js with front end frameworks like React and backend frameworks like Next, Django etc.
+          I am also very familar with python.
+          </div>
+      </section>
+    </>
   )
 }
